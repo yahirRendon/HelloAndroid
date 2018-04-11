04be1fe415e5c21b96a3007e6b7116f313211ac5
Yahir Rendon <yahir.rendon@snhu.edu>
2018-04-10 22:18:20 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.helloandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToWebsite(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.udacity.com"));
+        startActivity(intent);
+    }
+}
